92bcc777593146bda9a3dc35e60dda3989c3161a
Irina <ip_scheglova@mail.ru>
2022-11-27 22:32:24 +0500

    add Home Work Number 36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, -100, 101);
+WriteArray(numbers);
+int sum=0;
+for(int i=0; i<numbers.Length; i++)
+{
+    if(i%2==1) sum+=numbers[i];
+}
+
+Console.WriteLine(sum);
+
+
+//Используемые методы
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
